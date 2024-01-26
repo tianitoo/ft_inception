bda9091f63287c08392ae363c2d83ecc34661df9
tianitoo <hamza2nait@gmail.com>
2024-01-26 01:55:24 +0100

    fixed env vars for nginx

--- srcs/requirements/nginx/conf/create_conf.sh
+++ srcs/requirements/nginx/conf/create_conf.sh
@@ -1,22 +1,31 @@
+#!/bin/sh
+
+echo balbbddk
+echo $CERT
+cat << EOF > /etc/nginx/http.d/nginx.conf
 server {
     listen      443 ssl;
     server_name hnait.42.fr www.hnait.42.fr;
     root    /var/www/;
     index index.php;
-    ssl_certificate     /etc/nginx/ssl/hnait.42.fr.crt;
-    ssl_certificate_key /etc/nginx/ssl/hnait.42.fr.key;
+    ssl_certificate     ${CERT};
+    ssl_certificate_key ${KEY};
     ssl_protocols       TLSv1.2 TLSv1.3;
     ssl_session_timeout 10m;
     keepalive_timeout 70;
     location / {
-        try_files $uri /index.php?$args;
+        try_files \$uri /index.php?\$args;
     }
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
         fastcgi_pass wordpress:9000;
         fastcgi_index index.php;
         include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param PATH_INFO \$fastcgi_path_info;
     }
 }
+EOF
+
+
+cat /etc/nginx/http.d/nginx.conf
--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -5,7 +5,7 @@ wp config create        --allow-root \
         --dbname=$DB_NAME \
         --dbuser=$DB_USER \
         --dbpass=$DB_PASS \
-        --dbhost=mariadb:3306 
+        --dbhost=mariadb:3306
 
 wp core install --url=hnait.42.fr --title=hnait --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL
 wp user create auth_user auth@example.com --role=author --user_pass=$WP_USER_PASS
